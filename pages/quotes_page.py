5d665fb3d3621a81bbdd20a0dad27f36ba02710e
“tford-dev” <terranceford513@gmail.com>
2023-09-14 02:58:53 -0400

    Wrapped up quotes project

---
+++ pages/quotes_page.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup;
+
+from locators.quotes_page_locator import QuotePageLocators;
+from  parsers.quote import QuoteParser;
+
+class QuotesPage:
+    def __init__(self, page):
+        self.soup = BeautifulSoup(page, 'html.parser');
+
+    @property
+    def quotes(self):
+        locator = QuotePageLocators.QUOTE
+        quote_tags = self.soup.select(locator);
+        return [QuoteParser(e) for e in quote_tags];
